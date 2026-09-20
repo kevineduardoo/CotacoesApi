43b0e5e6418f04d5f322381bad102756b8509d76
kevineduardoo <ke47769@gmail.com>
2026-09-20 11:43:01 -0300

    Adiciona botão Authorize no Swagger para a API key

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using CotacoesApi.Data;
 using CotacoesApi.Services;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+        Name = "X-Api-Key",
+        Description = "Chave necessária apenas para POST /cotacoes/atualizar."
+    });
+
+    options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
+        [new OpenApiSecuritySchemeReference("ApiKey", document)] = []
+    });
+});
 builder.Services.AddMemoryCache();
 
 builder.Services.AddHttpClient<ICotacaoExternaService, CotacaoExternaService>();
